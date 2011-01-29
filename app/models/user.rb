fb5d805905c7373a9ef8093d6c1e7d123ae7629a
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-01-28 20:09:12 -0800

    Devise authenticable

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :authenticatable, :oauth2_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :twitter_oauth
+  devise :authenticatable, :oauth2_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :twitter_oauth
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :school, :password, :password_confirmation, :remember_me
