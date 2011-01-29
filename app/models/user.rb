c118fd7237900d7e020877c4452277e22e5ac7e2
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-01-28 21:31:31 -0800

    Twitter auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,12 @@ class User < ActiveRecord::Base
     User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
   end
 end 
+   def self.find_for_twitter_oauth(access_token, signed_in_resource=nil)
+  data = access_token['extra']['user_hash']
+  if user = User.find_by_email(data["email"])
+    user
+  else # Create an user with a stub password. 
+    User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
+  end
+end 
 end
